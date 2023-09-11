eeafa60ff867631ceb3503f5aa27248caae5d7dc
Esteban Solano <estebanjs029@gmail.com>
2023-09-10 21:12:36 -0600

    Tablero de tamaño dinamico funcional

--- app.js
+++ app.js
@@ -1,34 +1,47 @@
-var tablero = [];
-var tamano = 3;
+document.addEventListener('DOMContentLoaded', () => {
+    const botonTamano = document.getElementById('generar');
+    var tablero = [];
+    var tamano = 3;
+    var decremento = 0.3;
 
-window.onload = function(){
-    mostrarTablero();
-}
+    function crearTablero(){
+        for (let f = 0; f < tamano; f++) {
+            tablero[f] = [];
+            for (let c = 0; c < tamano; c++) {
+                tablero[f][c] = f*tamano + c + 1;
+            }
+        }
+        return tablero;
+    }
 
-
-
-function crearTablero(){
-    for (let f = 0; f < tamano; f++) {
-        tablero[f] = [];
-        for (let c = 0; c < tamano; c++) {
-            tablero[f][c] = f*tamano + c + 1;
+    function mostrarTablero(){
+        let tablero = crearTablero();
+        for (let f = 0; f < tamano; f++) {
+            for (let c = 0; c < tamano; c++) {
+                let casilla = document.createElement("div");
+                casilla.id = f.toString() + "-" + c.toString();
+                console.log(casilla.id);
+                let num = tablero[f][c];
+                casilla.innerHTML = num;
+                casilla.classList.add("casilla");
+                casilla.style.height = ((99-decremento) / tamano).toString() + "%";
+                casilla.style.width = ((99-decremento) / tamano).toString() + "%";
+                document.querySelector(".tablero").append(casilla);
+            }
         }
     }
-    return tablero;
-}
 
-function mostrarTablero(){
-    let tablero = crearTablero();
-    for (let f = 0; f < tamano; f++) {
-        for (let c = 0; c < tamano; c++) {
-            let casilla = document.createElement("div");
-            casilla.id = f.toString() + "-" + c.toString();
-            let num = tablero[f][c];
-            casilla.innerHTML = num;
-            casilla.classList.add("casilla");
-            casilla.style.height = (90/tamano).toString() + "%";
-            casilla.style.width = (90/tamano).toString() + "%";
-            document.querySelector(".tablero").append(casilla);
-        }
+    function limpiarTablero(){
+        let casillas = document.querySelectorAll(".casilla");
+        casillas.forEach(casilla => {
+            casilla.remove();
+        });
     }
-}
+
+    botonTamano.addEventListener("click", () =>{
+        limpiarTablero();
+        tamano = document.getElementById("tamano").value;
+        decremento = 0.315 * (tamano-3)
+        mostrarTablero();
+    });
+});
