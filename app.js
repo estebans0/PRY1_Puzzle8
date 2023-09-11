32543dda04606d834c1294eecf286676cb6d8030
Esteban Solano <estebanjs029@gmail.com>
2023-09-10 22:29:01 -0600

    Seleccionador de imagenes con imagenes de prueba

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const botonTamano = document.getElementById('generar');
+    const contenedorImagenes = document.querySelector(".contenedor_imagenes");
+    const botonMezclar = document.getElementById('mezclar');
+    const botonResolver = document.getElementById('resolver');
+    var imagenSeleccionada = document.querySelectorAll("#img1");
     var tablero = [];
     var tamano = 3;
     var decremento = 0.3;
@@ -20,7 +24,6 @@ document.addEventListener('DOMContentLoaded', () => {
             for (let c = 0; c < tamano; c++) {
                 let casilla = document.createElement("div");
                 casilla.id = f.toString() + "-" + c.toString();
-                console.log(casilla.id);
                 let num = tablero[f][c];
                 casilla.innerHTML = num;
                 casilla.classList.add("casilla");
@@ -38,10 +41,49 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    document.getElementById("img1").onclick = function() {
+        imagenSeleccionada = document.querySelectorAll("#img1");
+    };
+    document.getElementById("img2").onclick = function() {
+        imagenSeleccionada = document.querySelectorAll("#img2");
+    };
+    document.getElementById("img3").onclick = function() {
+        imagenSeleccionada = document.querySelectorAll("#img3");
+    };
+    document.getElementById("img4").onclick = function() {
+        imagenSeleccionada = document.querySelectorAll("#img4");
+    };
+    document.getElementById("img5").onclick = function() {
+        imagenSeleccionada = document.querySelectorAll("#img5");
+    };
+
+    function mostrarImagen(){
+        let casillas = document.querySelectorAll(".casilla");
+        let imagen = imagenSeleccionada[Math.floor(Math.random()*imagenSeleccionada.length)];
+        casillas.forEach(casilla => {
+            casilla.style.backgroundImage = "url(" + imagen.src + ")";
+            casilla.style.backgroundSize = "cover";
+            casilla.style.backgroundPosition = "center";
+        });
+    }
+
+    contenedorImagenes.addEventListener("click", () => {
+        mostrarImagen();
+    });
+
     botonTamano.addEventListener("click", () =>{
         limpiarTablero();
         tamano = document.getElementById("tamano").value;
         decremento = 0.3156 * (tamano-3)
         mostrarTablero();
+        mostrarImagen();
+    });
+
+    function mezclarTablero(){
+        
+    }
+
+    botonMezclar.addEventListener("click", () => {
+        mezclarTablero();
     });
 });
