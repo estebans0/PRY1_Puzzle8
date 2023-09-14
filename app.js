278198016e56db5c3e75df9c5104ab89b8811dbc
Esteban Solano <estebanjs029@gmail.com>
2023-09-14 11:14:59 -0600

    Inicio de implementación backtracking

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     var tablero = [];
     var tamano = 3;
     var decremento = 0.3;
+    var casillaActual = 0;
 
     function crearTablero(){
         for (let f = 0; f < tamano; f++) {
@@ -23,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
         for (let f = 0; f < tamano; f++) {
             for (let c = 0; c < tamano; c++) {
                 let casilla = document.createElement("div");
-                casilla.id = f.toString() + "-" + c.toString();
+                casilla.id = (f*tamano + c + 1).toString();
                 let num = tablero[f][c];
                 casilla.innerHTML = num;
                 casilla.classList.add("casilla");
@@ -61,9 +62,13 @@ document.addEventListener('DOMContentLoaded', () => {
         let casillas = document.querySelectorAll(".casilla");
         let imagen = imagenSeleccionada[Math.floor(Math.random()*imagenSeleccionada.length)];
         casillas.forEach(casilla => {
+            if (casilla.innerHTML == casillaActual.toString()) {
+                casilla.style.backgroundImage = "none";
+                casilla.style.backgroundColor = "grey";
+            } else {
             casilla.style.backgroundImage = "url(" + imagen.src + ")";
             casilla.style.backgroundSize = "cover";
-            casilla.style.backgroundPosition = "center";
+            casilla.style.backgroundPosition = "center"; }
         });
     }
 
@@ -74,7 +79,8 @@ document.addEventListener('DOMContentLoaded', () => {
     botonTamano.addEventListener("click", () =>{
         limpiarTablero();
         tamano = document.getElementById("tamano").value;
-        decremento = 0.3156 * (tamano-3)
+        decremento = 0.3156 * (tamano-3);
+        casillaActual = Math.floor(Math.random() * (tamano*tamano))+1;
         mostrarTablero();
         mostrarImagen();
     });
@@ -86,4 +92,52 @@ document.addEventListener('DOMContentLoaded', () => {
     botonMezclar.addEventListener("click", () => {
         mezclarTablero();
     });
+
+    botonResolver.addEventListener("click", () => {
+        
+    });
+
+    function moverCasilla(direccion){
+        let casilla = document.getElementById(casillaActual.toString());
+        let casillaArriba = document.getElementById((casillaActual-tamano).toString());
+        let casillaAbajo = document.getElementById((casillaActual+tamano).toString());
+        let casillaIzq = document.getElementById((casillaActual-1).toString());
+        let casillaDer = document.getElementById((casillaActual+1).toString());
+        if (direccion == "arriba" && casillaArriba != null) {
+            casilla.innerHTML = casillaArriba.innerHTML;
+            casillaArriba.innerHTML = casillaActual.toString();
+            casillaActual -= tamano;
+        } else if (direccion == "abajo" && casillaAbajo != null) {
+            casilla.innerHTML = casillaAbajo.innerHTML;
+            casillaAbajo.innerHTML = casillaActual.toString();
+            casillaActual += tamano;
+        } else if (direccion == "izq" && casillaIzq != null) {
+            casilla.innerHTML = casillaIzq.innerHTML;
+            casillaIzq.innerHTML = casillaActual.toString();
+            casillaActual -= 1;
+        } else if (direccion == "der" && casillaDer != null) {
+            casilla.innerHTML = casillaDer.innerHTML;
+            casillaDer.innerHTML = casillaActual.toString();
+            casillaActual += 1;
+        } else {
+
+        }
+        mostrarImagen();
+    }
+
+    document.addEventListener("keyup", (evento) => {
+        if (evento.code == "ArrowUp") {
+            moverCasilla("arriba");
+        } else if (evento.code == "ArrowDown") {
+            moverCasilla("abajo");
+        } else if (evento.code == "ArrowLeft") {
+            moverCasilla("izq");
+        } else if (evento.code == "ArrowRight") {
+            moverCasilla("der");
+        } else {
+
+        }
+    })
+
+    mostrarTablero();
 });
---
+++ backtracking.js
@@ -0,0 +1,111 @@
+var tableroFinal = [[1,2,3],[4,5,6],[7,8,0]];
+var tableroInicial = [
+    [1,4,3],
+    [5,0,8],
+    [7,6,3]];
+var arbol = [];
+
+main()
+
+function obtenerPadreHijos(n) {
+    let padre = Math.floor((n-1)/4);
+    let hijo1 = 4*n + 1;
+    let hijo2 = 4*n + 2;
+    let hijo3 = 4*n + 3;
+    let hijo4 = 4*n + 4;
+    return [padre, hijo1, hijo2, hijo3, hijo4];
+}
+
+function obtenerPosicionCero(tablero) {
+    for (let i = 0; i < tablero.length; i++) {
+        for (let j = 0; j < tablero[i].length; j++) {
+            if (tablero[i][j] == 0) {
+                return [i,j];
+            }
+        }
+    }
+}
+
+function generarPosiblesMovimientos(tablero) {
+    let posiblesMovimientos = [];
+    let posCero = obtenerPosicionCero(tablero);
+    let fila = posCero[0];
+    let columna = posCero[1];
+    if (fila > 0) {
+        posiblesMovimientos.push([fila-1, columna]);
+    } else {
+        posiblesMovimientos.push([-1,-1]);
+    }
+    if (fila < 2) {
+        posiblesMovimientos.push([fila+1, columna]);
+    } else {
+        posiblesMovimientos.push([-1,-1]);
+    }
+    if (columna > 0) {
+        posiblesMovimientos.push([fila, columna-1]);
+    } else {
+        posiblesMovimientos.push([-1,-1]);
+    }
+    if (columna < 2) {
+        posiblesMovimientos.push([fila, columna+1]);
+    } else {
+        posiblesMovimientos.push([-1,-1]);
+    }
+    return posiblesMovimientos;
+}
+
+function generarPosiblesTableros(tablero) {
+    let posiblesTableros = [];
+    let posiblesMovimientos = generarPosiblesMovimientos(tablero);
+    let posicionCero = obtenerPosicionCero(tablero);
+    for (let i = 0; i < posiblesMovimientos.length; i++) {
+        let fila = posiblesMovimientos[i][0];
+        let columna = posiblesMovimientos[i][1];
+        if (fila == -1 || columna == -1) {
+            posiblesTableros.push(-1);
+            continue;
+        }
+        let tableroCopia = tablero.map((arr) => arr.slice());
+        tableroCopia[fila][columna] = 0;
+        tableroCopia[posicionCero[0]][posicionCero[1]] = tablero[fila][columna];
+        posiblesTableros.push(tableroCopia);
+    }
+    return posiblesTableros;
+}
+
+function llenarArbol(posiblesTableros) {
+    for (let i = 0; i < posiblesTableros.length; i++) {
+        arbol.push(posiblesTableros[i]);
+    }
+}
+
+function main(){
+    // Agrega el tablero inicial al arbol
+    arbol.push(tableroInicial);
+
+    // Genera los posibles tableros a partir del tablero inicial y los agrega al arbol
+    let tableros = generarPosiblesTableros(tableroInicial);
+    llenarArbol(tableros);
+
+    // Genera los posibles tableros de cada posible tablero generado en el paso anterior y los agrega al arbol
+    tableros = generarPosiblesTableros(arbol[1]);
+    llenarArbol(tableros);
+
+    tableros = generarPosiblesTableros(arbol[2]);
+    llenarArbol(tableros);
+
+    tableros = generarPosiblesTableros(arbol[3]);
+    llenarArbol(tableros);
+
+    tableros = generarPosiblesTableros(arbol[4]);
+    llenarArbol(tableros);
+
+    // Imprime el arbol
+    console.log(arbol);
+
+    // Imprime el padre y los hijos de cada nodo
+    console.log(obtenerPadreHijos(1));
+    console.log(obtenerPadreHijos(2));
+    console.log(obtenerPadreHijos(3));
+    console.log(obtenerPadreHijos(4));
+}
